1ab85bd6005e4113faad65b4f56955da901b95b5
artem1911 <artem.tarasov.11@protonmail.ch>
2026-08-12 09:55:26 +0200

    Revert "test: add controlled broken-selector failure"
    This reverts commit 6f2484badef7d7a1aeae020c940734b11d7b4add.

--- cypress/e2e/pageObjects/subCategories.js
+++ cypress/e2e/pageObjects/subCategories.js
@@ -10,7 +10,7 @@ export class SubCategories {
     }
 
     getFastFood() {
-        return cy.contains('mat-checkbox', 'Fastfood');
+        return cy.contains('mat-checkbox', 'Fast food');
     }
 
     getFoodCourt() {
